82ec95efc91ae68487486a92f5967a56bc78ea85
Mehran safarzadeh <mr.atropates@gmail.com>
2025-09-07 11:33:17 +0330

    fix securekey bug, and static file problems

--- hiddify/entrypoint.sh
+++ hiddify/entrypoint.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-# Generate SECRET_KEY dynamically
-export SECRET_KEY=$(python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
+ENV_FILE=".env"
+# check if .env file exists, if not create it
+if ! grep -q "SECRET_KEY=" "$ENV_FILE"; then
+    echo "Generating new SECRET_KEY..."
+    # generate a new SECRET_KEY and append it to the .env file
+    SECRET_KEY=$(python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
+    echo "SECRET_KEY=$SECRET_KEY" >> "$ENV_FILE"
+else
+    echo "SECRET_KEY already exists in .env, skipping generation."
+fi
+
+# Load environment variables from .env file
+export $(grep -v '^#' "$ENV_FILE" | xargs)
+
 
 # Run database migrations
 python manage.py makemigrations
 python manage.py migrate
 
 # Collect static files
+mkdir -p /var/www/static
+chown -R www-data:www-data /var/www/static
 python manage.py collectstatic --no-input --clear
 
 # Start Gunicorn server
--- hiddify/hiddify/settings.py
+++ hiddify/hiddify/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -154,18 +155,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# You can still have STATICFILES_DIRS for development purposes
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'accounts/static'),] # Add the accounts app static files directory to the list
 
 STATIC_ROOT = BASE_DIR / 'static' 
-MEDIA_ROOT = BASE_DIR / 'media' 
-
-
+# STATIC_ROOT = '/var/www/static'
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Media files
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media' 
 MEDIA_URL = '/media/'
+
+
+
 
 
 # Default primary key field type
